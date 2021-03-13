4d8886ff6536ccf952f9f59b4ee497c8f8631461
salsifitewn <salsifite@gmail.com>
2021-03-13 19:38:02 +0100

    :wrench: update linter https://github.com/vuejs/eslint-config-typescript/blob/e7e184c/index.js https://github.com/prettier/eslint-plugin-prettier#recommended-configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true,
     jest: true,
   },
-  parser: '@typescript-eslint/parser',
   extends: [
     'airbnb-typescript/base',
-    'prettier/@typescript-eslint',
     'plugin:vue/vue3-recommended', // base Vue 3
     'plugin:prettier/recommended', // enable eslint-plugin-prettier, prettier as error, enables eslint-config-prettier
-    'prettier/vue', // add compatibilty with eslint-plugin-prettier (remove vue plugin conflict)
   ],
-  plugins: ['@typescript-eslint', 'import', 'vue'],
+  plugins: ['@typescript-eslint', 'import'],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
+    extraFileExtensions: ['.vue'],
   },
 }
